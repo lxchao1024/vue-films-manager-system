2e5040d033c0946ebf0019267ae52f88372a5d03
lixiangchao <lixiangchao@iqiju.com>
2021-01-22 11:12:02 +0800

    add library

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -69,6 +69,9 @@
     width: 100%;
     background-size: cover;
     position: fixed;
+    align-items: center;
+    align-content: center;
+    flex: 1;
   }
    body{
      margin: 0;
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '@/components/Login'
 import AppIndex from '@/components/home/AppIndex'
+import Home from '@/components/Home'
+import LibraryIndex from '@/components/library/LibraryIndex'
 
 Vue.use(Router)
 
@@ -10,17 +12,34 @@ export default new Router({
   mode: 'history',
   routes: [
     {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      // home页面并不需要被访问
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          meta: {
+            requireAuth: true
+          }
+        },
+        {
+          path: '/library',
+          name: 'Library',
+          component: LibraryIndex,
+          meta: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
+    {
       path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex,
-      meta: {
-        requireAuth: true
-      }
     }
   ]
 })
